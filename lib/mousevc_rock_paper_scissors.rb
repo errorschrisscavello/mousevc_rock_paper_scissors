b9345930928a68fb1d838f09cf0fceb4c61e037a
Chris Scavello <bideowego@gmail.com>
2015-08-24 04:31:36 -0400

    Bug fix: wrap Game in module

--- lib/mousevc_rock_paper_scissors.rb
+++ lib/mousevc_rock_paper_scissors.rb
@@ -4,15 +4,17 @@ require_relative 'mousevc_rock_paper_scissors/version.rb'
 require_relative 'mousevc_rock_paper_scissors/game_controller.rb'
 require_relative 'mousevc_rock_paper_scissors/game_model.rb'
 
-class Game < Mousevc::App
-	def initialize
-		super(
-			:controller => 'GameController',
-			:model => 'GameModel',
-			:views => 'lib/views',
-			:action => :menu,
-			:looping => true,
-			:system_clear => true
-		)
+module MousevcRockPaperScissors
+	class Game < Mousevc::App
+		def initialize
+			super(
+				:controller => 'GameController',
+				:model => 'GameModel',
+				:views => 'lib/views',
+				:action => :menu,
+				:looping => true,
+				:system_clear => true
+			)
+		end
 	end
 end
